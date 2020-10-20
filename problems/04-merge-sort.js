c55515d79994cf771805c99aa5fe4350d1a78ff8
Joshua <j.maxwell.alan@gmail.com>
2020-10-20 13:08:40 -0400

    Completed merge sort

--- problems/04-merge-sort.js
+++ problems/04-merge-sort.js
@@ -2,11 +2,35 @@
 
 function merge(array1, array2) {
   // your code here
+  let array = [];
+  // [1, 3, 5] [2, 4, 6]
+  while (array1.length && array2.length) {
+    if (array1[0] < array2[0]) {
+      array.push(array1.shift());
+    } else if (array2[0] <= array1[0]) {
+      array.push(array2.shift());
+    }
+  }
+  if (array1.length) {
+    array.push(...array1);
+  } else if (array2.length) {
+    array.push(...array2)
+  }
+  return array;
 }
+
 
 function mergeSort(array) {
   // your code here
+  if (array.length <= 1) return array;
+
+  let midPoint = Math.floor(array.length / 2);
+  let left = mergeSort(array.slice(0,midPoint));
+  let right = mergeSort(array.slice(midPoint));
+  return merge(left,right);
 }
+
+console.log(mergeSort([1,3,9,4,3,8,7,3,1]));
 
 module.exports = {
   merge,
