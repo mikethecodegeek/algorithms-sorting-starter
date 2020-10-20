b8c1cbf7a5ef07c56c94b220e9e6efa927cbef56
mike <mikethecodegeek@gmail.com>
2020-10-20 09:46:14 -0700

    insertion sort

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,20 @@
 // Implement Insertion Sort
+let array = [2, -1, 4, 3, 7, 3];
 
 function insertionSort(list) {
   // your code here
+  for (let a=1; a<= list.length-1;a++) {
+    let w;
+    let currentEl = list[a];
+    for (w=a-1; w>-1 && currentEl<list[w];w--) {
+      list[w+1]=list[w];
+    }
+    list[w+1]=currentEl;
+  }
+
 }
+
+insertionSort(array);
 
 module.exports = {
   insertionSort
